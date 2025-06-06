0f1dfea4122e828f71c778ac289277732fc82c1f
Stalin Carruyo <145412261+ispulzar@users.noreply.github.com>
2025-06-06 15:55:07 -0400

    Update Navbar.tsx
    cuenta por pagar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,7 +35,7 @@ const allLinks = [
             { to: '/cajeros', label: 'Vendedores', permiso: 'acceso_admin' },
             { to: '/comisiones', label: 'Comisiones Por Turno', permiso: 'acceso_admin' },
             { to: '/comisionesgenerales', label: 'Comisiones Generales', permiso: 'acceso_admin' },
-            { to: '/cuentasporpagar', label: 'Cuentas por Pagar', permiso: 'acceso_admin' },
+            { to: '/cuentasporpagar', label: 'Agregar Cuenta Por Pagar', permiso: 'agregar_cuadre' },
             { to: '/vercuentasporpagar', label: 'Ver Cuentas por Pagar', permiso: 'acceso_admin' },
             { to: '/agregarinventariocosto', label: 'Agregar Costo Inv', permiso: 'acceso_admin' },
             { to: '/verinventarios', label: 'Ver Inventarios', permiso: 'acceso_admin' },
